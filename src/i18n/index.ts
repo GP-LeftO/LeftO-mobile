d8b15c7f9f8dfcd90d2410491f73af4bdcf784b7
TalaAlhendi <talaalhendiuni4@gmail.com>
2026-03-27 00:43:23 +0200

    feat: setup i18next with Arabic and English translations

---
+++ src/i18n/index.ts
@@ -0,0 +1,35 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import * as Localization from 'expo-localization';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import en from './en.json';
+import ar from './ar.json';
+
+const LANGUAGE_KEY = 'user-language';
+
+const initI18n = async () => {
+  let savedLanguage = await AsyncStorage.getItem(LANGUAGE_KEY);
+
+  if (!savedLanguage) {
+    savedLanguage = Localization.getLocales()[0]?.languageCode ?? 'en';
+  }
+
+  i18n
+    .use(initReactI18next)
+    .init({
+      resources: {
+        en: { translation: en },
+        ar: { translation: ar },
+      },
+      lng: savedLanguage,
+      fallbackLng: 'en',
+      interpolation: {
+        escapeValue: false,
+      },
+    });
+};
+
+initI18n();
+
+export default i18n;
